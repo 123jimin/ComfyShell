b16eac8a2e6dbffd392f0a0fd86131e9fe5b26eb
Jimin Park <pjm950@gmail.com>
2023-12-03 14:24:38 +0900

    Setting toml imports.

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import { useEffect } from "preact/hooks";
 import "./app.css";
 
+import default_settings from "./default-settings.toml";
+console.log(default_settings);
+
 export function App() {
     useEffect(() => {
-        
+
     }, []);
 
     return <p>Hello, world!</p>;
